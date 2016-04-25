2201108ff952b93f88ed41a52fc83cdc2ce881c7
robertfiz <robertfiz@users.noreply.github.com>
2016-04-25 12:15:51 -0300

    Update jugandoConGoogle2.py

--- jugandoConGoogle2.py
+++ jugandoConGoogle2.py
@@ -9,42 +9,44 @@ class JugandoConGoogle2(basesinfonierbolt.BaseSinfonierBolt):
 
 	def userprepare(self):
 		self.hostname = self.getParam("hostname")
-
-    	def userprocess(self):
-    		h=self.getField(self.hostname)
-    		url = 'https://www.google.com/xhtml?'		
-		q = 'site:'+h
-		start=0
-		num=100
-		gws_rd = 'ssl'
-		query_string = { 'q':q, 'start':start, 'num':num, 'gws_rd':gws_rd }
-		data = urllib.urlencode(query_string)
-		url = url + data
-		headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)', 'Referer' : 'http://127.0.0.1/'} # $
+		self.h=self.getField(self.hostname)
+    		self.url = 'https://www.google.com/xhtml?'		
+		self.q = 'site:'+h
+		self.start=0
+		self.num=100
+		self.gws_rd = 'ssl'
+		self.query_string = { 'q':q, 'start':self.start, 'num':self.num, 'gws_rd':self.gws_rd }
+		self.data = urllib.urlencode(self.query_string)
+		sel.url = self.url + self.data
+		self.headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)', 'Referer' : 'http://127.0.0.1/'} # $
 		try:
-        		req = urllib2.Request(url, None, headers)
-                	google_reply = urllib2.urlopen(req).read()
-                	regex = '<h3 class="r"><a href="/url(.+?)">'
-                	pattern = re.compile(regex)
-                	url_links = re.findall(pattern, google_reply)
+        		self.req = urllib2.Request(self.url, None, self.headers)
+                	self.google_reply = urllib2.urlopen(req).read()
+                	self.regex = '<h3 class="r"><a href="/url(.+?)">'
+                	self.pattern = re.compile(regex)
+                	self.url_links = re.findall(pattern, google_reply)
         	except urllib2.URLError:
                 	pass
 
-		hosts=[]
-		ips=[]
+		self.hosts=[]
+		self.ips=[]
+		
+
+    	def userprocess(self):
+    	
 	
-		for url in url_links:
-			url2=url.strip('?q=')
+		for self.url in self.url_links:
+			self.url2=url.strip('?q=')
 			try:
-				d=urlparse(url2)
-				if d.netloc not in hosts:
-					hosts.append(d.netloc)	
+				self.d=urlparse(sel.url2)
+				if self.d.netloc not in self.hosts:
+					self.hosts.append(self.d.netloc)	
                 	except socket.error:
                         	pass
 
 
-        	for host in hosts:
-        		self.addField(json.dumps(host))
+        	for self.host in self.hosts:
+        		self.addField(json.dumps(self.host))
 	   	self.emit()
                
 JugandoConGoogle2().run()
